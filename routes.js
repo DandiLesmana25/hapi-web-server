c8708f70b1195ea80d2997d4218db8ee02acff27
Dandi Lesmana <dandilesmana.if21@gmail.com>
2023-04-06 06:33:47 -0700

    add path parameter

---
+++ routes.js
@@ -0,0 +1,53 @@
+// route configuration
+
+// * = route dapat diakses menggunakan seluruh method HTTP. 
+
+const routes = [{
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method';
+        },
+    },
+
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const { name = "stranger" } = request.params;
+            return `Hello, ${name}!`;
+
+        }
+    },
+
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        },
+    },
+];
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-console.log('Halo, kita akan belajar membuat server menggunakan Hapi');
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async() => {
+    //create HTTP server
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+
+    // server.route({
+    //     method: 'GET',
+    //     path: '/',
+    //     handler: (request, h) => {
+    //         return 'Hello World!';
+    //     }
+    // });
+
+    server.route(routes);
+
+    //running server (call server.start use await)
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
